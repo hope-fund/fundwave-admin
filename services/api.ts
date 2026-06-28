66fa9892c83488961733559309ceab01947648ae
Hope Fund <hopeundraise@gmail.com>
2026-06-28 03:11:02 +0100

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://fundwave-bd.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
